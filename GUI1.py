f831ec61943afa3e655492f123b0aa3767575094
GitGodess02 <156833343+GitGodess02@users.noreply.github.com>
2024-01-24 15:20:22 +0000

    Update GUI1.py

--- GUI1.py
+++ GUI1.py
@@ -35,6 +35,53 @@ dropSearchBy.grid(row=0, column=1, sticky=E, pady=4)
 entrySearchVal.grid(row=1, column=1, sticky=E, pady=4)
 buttonSearch.grid(row=2, column=0, sticky=W, pady=2)
 
+food_delivery = Treeview(main)
+food_delivery['columns'] = ('order_id', 'date', 'restaurant_name', 'restaurant_id', 'restaurant_postcode', 'dish_id', 'dish_name', 'price', 'total_price', 'delivery_postcode', 'contact_number', 'deliver_by', 'house_number')
+
+food_delivery.column("#0", width=0,  stretch=NO)
+food_delivery.column("order_id",anchor=CENTER, width=80)
+food_delivery.column("date",anchor=CENTER, width=80)
+food_delivery.column("restaurant_name",anchor=CENTER,width=80)
+food_delivery.column("restaurant_id",anchor=CENTER,width=80)
+food_delivery.column("restaurant_postcode",anchor=CENTER,width=80)
+food_delivery.column("dish_id",anchor=CENTER,width=80)
+food_delivery.column("dish_name",anchor=CENTER,width=80)
+food_delivery.column("price",anchor=CENTER,width=80)
+food_delivery.column("total_price",anchor=CENTER,width=80)
+food_delivery.column("delivery_postcode",anchor=CENTER,width=80)
+food_delivery.column("contact_number",anchor=CENTER,width=80)
+food_delivery.column("deliver_by",anchor=CENTER,width=80)
+food_delivery.column("house_number",anchor=CENTER,width=80)
 
 
+food_delivery.heading("#0",text="",anchor=CENTER)
+food_delivery.heading("order_id",text="ID",anchor=CENTER)
+food_delivery.heading("date",text="Date",anchor=CENTER)
+food_delivery.heading("restaurant_name",text="Restaurant Name",anchor=CENTER)
+food_delivery.heading("restaurant_id",text="Restaurant ID",anchor=CENTER)
+food_delivery.heading("restaurant_postcode",text="Restaurant Postcode",anchor=CENTER)
+food_delivery.heading("dish_id",text="Dish ID",anchor=CENTER)
+food_delivery.heading("dish_name",text="Dish Name",anchor=CENTER)
+food_delivery.heading("price",text="Price",anchor=CENTER)
+food_delivery.heading("total_price",text="Total Price",anchor=CENTER)
+food_delivery.heading("delivery_postcode",text="Delivery Postcode",anchor=CENTER)
+food_delivery.heading("contact_number",text="Contact Number",anchor=CENTER)
+food_delivery.heading("deliver_by",text="Deliver By",anchor=CENTER)
+food_delivery.heading("house_number",text="House Number",anchor=CENTER)
+
+
+food_delivery.insert(parent='',index='end',iid=0,text='',
+values=('1','Ninja','101','Oklahoma', 'Moore'))
+food_delivery.insert(parent='',index='end',iid=1,text='',
+values=('2','Ranger','102','Wisconsin', 'Green Bay'))
+food_delivery.insert(parent='',index='end',iid=2,text='',
+values=('3','Deamon','103', 'California', 'Placentia'))
+food_delivery.insert(parent='',index='end',iid=3,text='',
+values=('4','Dragon','104','New York' , 'White Plains'))
+food_delivery.insert(parent='',index='end',iid=4,text='',
+values=('5','CrissCross','105','California', 'San Diego'))
+food_delivery.insert(parent='',index='end',iid=5,text='',
+values=('6','ZaqueriBlack','106','Wisconsin' , 'TONY'))
+
+food_delivery.grid(row= 5, column= 0, rowspan=3)
 main.mainloop()
